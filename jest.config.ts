a28ada10306ae144fc0661f0bfafcf1582e04539
xsadia <felipe.rosa.arthur27@gmail.com>
2022-12-10 23:18:50 -0300

    feat(ci): i'm once again asking for you to work

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
-  rootDir: "./",
+  rootDir: "src",
   roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
   transform: {
